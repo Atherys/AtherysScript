8f015915a9fa513214438ded686976bd95d96a8f
Liam <liamreadsspam@gmail.com>
2018-08-02 08:09:56 -0300

    Start Sound functions

---
+++ src/main/java/com/atherys/script/js/library/sound/SoundLibrary.java
@@ -0,0 +1,12 @@
+package com.atherys.script.js.library.sound;
+
+import com.atherys.script.api.library.LibraryExtension;
+import com.atherys.script.api.library.ScriptLibrary;
+
+public class SoundLibrary implements LibraryExtension {
+
+    @Override
+    public void extend(ScriptLibrary library) {
+
+    }
+}
