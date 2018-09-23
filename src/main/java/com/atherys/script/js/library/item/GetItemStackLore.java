a8aae65ca3eb171fdf002a8ec855d7acc0d88035
Liam <liamreadsspam@gmail.com>
2018-09-23 11:58:09 -0300

    Try returning a list

--- src/main/java/com/atherys/script/js/library/item/GetItemStackLore.java
+++ src/main/java/com/atherys/script/js/library/item/GetItemStackLore.java
@@ -4,14 +4,15 @@ import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 
+import java.util.List;
 import java.util.function.Function;
 
 /**
  * @jsfunc
  */
-public class GetItemStackLore implements Function<ItemStack, Text[]> {
+public class GetItemStackLore implements Function<ItemStack, List<Text>> {
     @Override
-    public Text[] apply(ItemStack itemStack) {
-        return itemStack.get(Keys.ITEM_LORE).map(list -> (Text[]) list.toArray()).orElse(null);
+    public List<Text> apply(ItemStack itemStack) {
+        return itemStack.get(Keys.ITEM_LORE).orElse(null);
     }
 }
