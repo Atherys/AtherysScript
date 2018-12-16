2742c1f6825d18a95ac0e628e01f381c36cb6152
Liam <liamreadsspam@gmail.com>
2018-12-16 12:49:06 -0400

    Document new textOf and remove unneeded text functions

--- src/main/java/com/atherys/script/js/library/text/TextOf.java
+++ src/main/java/com/atherys/script/js/library/text/TextOf.java
@@ -1,18 +1,25 @@
 package com.atherys.script.js.library.text;
 
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.serializer.TextSerializers;
 
 import java.util.function.Function;
 
 /**
  * @jsfunc
  */
-public class TextOf implements Function<Object[], Text> {
+public class TextOf implements Function<String, Text> {
 
-    TextOf() {}
-
+    /**
+     * Formats a `String` into a `Text` object using Minecraft formatting codes. See [here](https://minecraft.gamepedia.com/Formatting_codes) for
+     * the available codes. However, you must use an ampersand instead of the section sign used on the wiki page.
+     *
+     * @ex var message = textOf("&4This text is red!");
+     * @param text The formatted string
+     * @return The formatted text.
+     */
     @Override
-    public Text apply(Object[] objects) {
-        return Text.of(objects);
+    public Text apply(String text) {
+        return TextSerializers.FORMATTING_CODE.deserialize(text);
     }
 }
